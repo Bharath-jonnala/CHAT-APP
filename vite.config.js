87aa64b3b0b6367acd63474eee2db9e197c9c00e
Your Name <you@example.com>
2025-07-27 14:25:31 +0530

    chnaged

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,13 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'gh-pages' ? '/CHAT-APP/' : '/', // ✅ Adjusts automatically
+  base: mode === "gh-pages" ? "/chat-app/" : "/", // 👈 for GitHub Pages
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src")
+    }
+  }
 }));
